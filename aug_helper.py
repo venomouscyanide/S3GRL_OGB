f0840103f4296cf1280a612bd44e9d07b5de575a
Paul Louis <iampaullouis@gmail.com>
2023-03-14 11:19:01 -0400

    Edge features for ogbl-ppa

--- aug_helper.py
+++ aug_helper.py
@@ -1,10 +1,12 @@
-# Adapted from: https://github.com/chuanqichen/cs224w/blob/aeebce6810221bf04a9a14d8d4369be76691b608/ddi/gnn_augmented_node2vec_random.py#L151
 import ast
 import pickle
 
+import numpy as np
 import torch
 from networkx import degree_centrality
+from torch_geometric.data import DataLoader
 from torch_geometric.utils import degree, to_networkx
+from tqdm import tqdm
 
 CLUSTER_FILENAME = "./features/clustering.txt"
 PAGERANK_FILENAME = "./features/pagerank.txt"
@@ -13,6 +15,7 @@ CENTRALITY_FILENAME = "./features/centrality.pkl"
 
 
 def get_features(n_nodes, data):
+    # Adapted from: https://github.com/chuanqichen/cs224w/blob/aeebce6810221bf04a9a14d8d4369be76691b608/ddi/gnn_augmented_node2vec_random.py#L151
     with open(PAGERANK_FILENAME, "r") as f:
         contents = f.read()
         pagerank_dict = ast.literal_eval(contents)
@@ -34,3 +37,26 @@ def get_features(n_nodes, data):
     ones = torch.ones((n_nodes, 1))
     features = torch.cat((ones, pagerank_vals, cluster_vals, centrality_vals, degree_vals), 1)
     return features
+
+
+def resource_allocation(adj_matrix, link_list, batch_size=1):
+    # adapted from: https://github.com/lustoo/OGB_link_prediction/blob/main/PPA/generate_feature.py
+    A = adj_matrix
+    w = 1 / A.sum(axis=0)
+    w[np.isinf(w)] = 0
+    temp = np.log(A.sum(axis=0))
+    temp = 1 / temp
+    temp[np.isinf(temp)] = 1
+    D = A.multiply(w).tocsr()
+
+    link_index = link_list
+    link_loader = DataLoader(range(link_index.shape[1]), batch_size)
+    ra = []
+
+    print("Calculating ra values for edges")
+    for idx in tqdm(link_loader, ncols=70):
+        src, dst = link_index[0, idx], link_index[1, idx]
+        ra.append(np.array(np.sum(A[src].multiply(D[dst]), 1)).flatten())
+
+    ra = np.concatenate(ra, 0)
+    return torch.FloatTensor(ra)
